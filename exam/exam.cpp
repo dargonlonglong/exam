77cfb51a6201ad21860c2a6be53be015bbdfd3fb
林佑龍(dargonlonglong) <4b3g0146@office.stust.edu.tw>
2024-11-12 21:42:18 +0800

    改為計算 1 到 n 之間偶數和的程式
    這段程式碼的變更主要是將原本的"Hello World!" 輸出改為一個程式，該程式會要求使用者輸入一個正整數 n，然後計算並輸出 1 到 n 之間偶數的和。程式使用了三種不同的迴圈來計算這個和，分別是 for 迴圈、while 迴圈和 do-while 迴圈。

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -3,6 +3,39 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    int n = 1;
+    int sum = 0;
+    cout << "請輸入一個正整數n: ";
+    cin >> n;
+
+    // for loop
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) sum += i;
+    }
+
+    sum = 0;
+    for (int i = 2; i <= n; i += 2) {
+      sum += i;
+    }
+    cout << " For loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+
+    // while loop
+    int i = 1;
+    sum = 0;
+    while (i <= n) {
+      if (i % 2 == 0) sum += i;
+      i++;
+    }
+    cout << " While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+
+    // do while loop
+    int i = 2;
+    sum = 0;
+    do {
+        sum += i;
+        i += 2;
+    } while (i <= n);
+    cout << " Do-While loop: 1~" << n << "之間所有的偶數的和為: " << sum << endl;
+
 }
 
