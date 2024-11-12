b8cc82ad705ddc2f848e235525adabd265adc921
林佑龍(dargonlonglong) <4b3g0146@office.stust.edu.tw>
2024-11-12 22:22:59 +0800

    新增階乘計算功能並修改 main 函數
    新增遞迴函數ˋfactorialˋ計算階乘。修改ˋaminˋ函數，要求使用者輸入正整數。在ˋmainˋ函數中，新增非遞迴和遞迴兩種方法計算階乘並輸出結果。

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -1,8 +1,29 @@
 ﻿#include <iostream>
 using namespace std;
 
+int factorial(int n) {
+    if (n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int num;
+
+    cout << "輸入一個正整數: ";
+    cin >> num;
+
+    //非遞迴計算n!
+    int result = 1;
+    for (int i = num; i <=num; i++)
+    {
+        //result *= i;
+        result = result * i;
+    }
+    cout << "非遞迴版本 : " << num << " ! = " << result << endl;
+
+    //遞迴計算n!
+    result = factorial(num);
+    cout << "遞迴版本 : " << num << " ! = " << result << endl;
 }
 
