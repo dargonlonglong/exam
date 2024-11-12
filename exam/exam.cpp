8086e16a43c3ff6598ecdde33e2f0c30f54cba5f
林佑龍(dargonlonglong) <4b3g0146@office.stust.edu.tw>
2024-11-12 20:27:39 +0800

    使用 switch 語句優化月份判斷邏輯
    將月份判斷的邏輯從一系列的 if-else 語句改為 switch 語句，使代碼更為簡潔和易讀。刪除了錯誤和正確的註釋，並使用 switch 語句來判斷月份並設置對應的季節值 result 。根據月份的範圍，將 3、4、5月設置為"春季"，6、7、8月設置為"夏季"，9、10、11月設置為"秋季"，其餘月份設置為"冬季"。最後，輸出月份和對應的季節。

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -12,10 +12,26 @@ int main()
 		cout << "請輸入有效的月份" << endl;
 	}
 	else {
-		if (month >= 3 && month <= 5) result = "春季";
-		else if (month >= 6 && month <= 8) result = "夏季";
-		else if (month >= 9 && month <= 11) result = "秋季";
-		else result = "冬季";
+		switch (month) {
+		case 3:
+		case 4:
+		case 5:
+			result = "春季";
+			break;
+		case 6:
+		case 7:
+		case 8:
+			result = "夏季";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			result = "秋季";
+			break;
+		default:
+			result = "秋季";
+			break;
+		}
 		cout << month << "月是" << result << endl;
 	}
 }
